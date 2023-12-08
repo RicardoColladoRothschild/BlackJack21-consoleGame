5c3241b7b8262f1a044d61af1f3d6539f66ce583
ricardocollado <ricardocolladorothschild@gmail.com>
2023-12-07 22:42:07 -0400

    add menu option, and working currenly with cards selection

--- BlackJackImprove/Program.cs
+++ BlackJackImprove/Program.cs
@@ -2,8 +2,10 @@
 int cartasJugador = 0;
 int cartasDealer = 0;
 int coins = 0;
+int option = 1;
 string? nombre = "";
-    
+
+Random randomly = new Random();
     
     
 Console.WriteLine("¿Cual es su nombre?");
@@ -15,11 +17,33 @@ while (true) {
     Console.WriteLine("¿Cuantas fichas desea obtener?");
     coins = int.Parse(Console.ReadLine());
     Console.WriteLine("**************************************************************");
-    for (int i = 0; i < coins; i ++) {
+    for (int i = 0; i <= coins; i ++) {
 
-        Console.WriteLine("¿Cual juego deseas jugar?");
-        Console.WriteLine("1) BlackJack 21");
-    
+        cartasDealer = 0;
+        cartasJugador = 0;
+        
+        option = Convert.ToInt32(Console.ReadLine());
+
+        switch (option) 
+        {
+            //menu
+            case 0:
+
+                Console.WriteLine("¿Cual juego deseas jugar? (Seleccione un numero)");
+                Console.WriteLine("1) BlackJack 21");
+                option = Convert.ToInt32(Console.ReadLine());
+                i += 1;
+                break;
+            case 2:
+                Console.WriteLine($"Bienvenido/a {nombre}, ¿deseas tomar una carta?");
+                do 
+                { 
+                        
+                } while ();
+                break;
+
+        }
+        
     }
 
 }
