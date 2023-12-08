73ef74e50240f534471121bc270846d2089b1501
ricardocollado <ricardocolladorothschild@gmail.com>
2023-12-07 21:37:20 -0400

    Add project files.

---
+++ BlackJackImprove/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int cartasJugador = 0;
+int cartasDealer = 0;
+int coins = 0;
+string? nombre = "";
+    
+    
+    
+Console.WriteLine("¿Cual es su nombre?");
+nombre = Console.ReadLine();
+
+while (true) {
+
+    Console.WriteLine($"********* Bienvenido al Super Casino, {nombre} ************\n");
+    Console.WriteLine("¿Cuantas fichas desea obtener?");
+    coins = int.Parse(Console.ReadLine());
+    Console.WriteLine("**************************************************************");
+    for (int i = 0; i < coins; i ++) {
+
+        Console.WriteLine("¿Cual juego deseas jugar");
+    
+    }
+
+}
